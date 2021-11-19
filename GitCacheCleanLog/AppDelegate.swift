51d37732e2581b14d42cd782f203e27f5243e829
vistaLin <18349261082@163.com>
2021-11-19 14:38:53 +0800

    2

--- GitCacheCleanLog/AppDelegate.swift
+++ GitCacheCleanLog/AppDelegate.swift
@@ -12,6 +12,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
     var test = 1
+    var test2 = 2
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
