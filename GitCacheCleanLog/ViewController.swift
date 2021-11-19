3695d1c58e49a09cc2aa4388f84e00a4cda938c9
wangkt <gw00249354@gwm.cn>
2021-11-19 14:00:21 +0800

    first commit

---
+++ GitCacheCleanLog/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  GitCacheCleanLog
+//
+//  Created by Rookie on 2021/11/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = UIColor.red
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
